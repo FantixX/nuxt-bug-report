19ca2ea5166afee74348931a65ac3628c91a1c1a
Luis Abeler <la@abeler-online.de>
2024-05-18 00:59:16 +0200

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+    <NuxtLink to="/page2">
+        Link
+    </NuxtLink>
+</template>
---
+++ pages/page2.vue
@@ -0,0 +1,12 @@
+<script lang="ts" setup>
+
+/// This is not executed the first time a <NuxtLink> points to this page. 
+/// Runs correctly when accessed by URL in the Browsert and on every reload.
+console.log("runing <script setup>...");
+
+
+</script>
+
+<template>
+    <div>Page 2</div>
+</template>
